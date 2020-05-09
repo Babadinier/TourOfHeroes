f2c202526b216ecc8f59b9a9600d9a53945ba136
Guillaume B <g.badinier@hotmail.fr>
2020-05-09 16:55:37 +0200

    Add CRUD operations for HeroService

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,6 +13,10 @@ export class HeroService {
 
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+  
   constructor(private messageService: MessageService,
     private http: HttpClient) { }
 
@@ -33,7 +37,30 @@ export class HeroService {
       );
   }
 
-    /** Log a HeroService message with the MessageService */
+  updateHero(hero: Hero) {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
+  deleteHero(hero: Hero | number): Observable<Hero> {
+    const id = typeof hero === 'number' ? hero : hero.id;
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+  
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
   }
